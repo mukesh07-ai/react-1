90de274e523e525c673c5b5fb32df2de58b9c84b
MUKESH KUMAR VISHWAS <114977531+mukeshvishwas@users.noreply.github.com>
2024-01-12 11:16:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <h1>MUKESH</h1>
+    <input type = 'text' />
+    <br/>
+    <input type = 'date' />
+    <br/>
+    <button>submit</button>
+
+    </div>
+  );
+}
+
+export default App;
